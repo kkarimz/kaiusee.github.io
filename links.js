c9f5dd4fce28c33b65f3ae7181a4daaf0595f080
Kais Karim <kaiusee@gmail.com>
2026-04-28 12:48:09 -0400

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,5 @@
 const LINKS = {
-    "Paste Bin": "http://kais.local:5050/",
+    "PasteBin": "http://kais.local:5050/",
     "Weather Dashboard": "http://kais.local:89",
     "Quran 1": "https://soundcloud.com/user-770372009/sets/quran/s-zgvBuYYwQOt",
     "Quran 2": "https://soundcloud.com/s-a-550587357/ot51jzawhoks"
