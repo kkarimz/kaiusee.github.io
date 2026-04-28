c8ccb5eb2ec06694893a8e4d3ce3719390c6ee78
Kais Karim <kaiusee@gmail.com>
2026-04-28 12:00:26 -0400

    Create links.js

---
+++ links.js
@@ -0,0 +1,7 @@
+const LINKS = {
+    "Weather Dashboard": "dashboard/index.html",
+    "Quran Collection 1": "https://soundcloud.com/user-770372009/sets/quran/s-zgvBuYYwQOt",
+    "Quran Collection 2": "https://soundcloud.com/s-a-550587357/ot51jzawhoks",
+    "Personal Projects": "#",
+    "Coming Soon": "#"
+};
