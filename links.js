fb9cf724f293b91dbbaefda6fd8f7c8ac3c5c09d
Kais Karim <kaiusee@gmail.com>
2026-04-28 12:06:14 -0400

    Update links.js

--- links.js
+++ links.js
@@ -1,7 +1,6 @@
 const LINKS = {
-    "Weather Dashboard": "dashboard/index.html",
-    "Quran Collection 1": "https://soundcloud.com/user-770372009/sets/quran/s-zgvBuYYwQOt",
-    "Quran Collection 2": "https://soundcloud.com/s-a-550587357/ot51jzawhoks",
-    "Personal Projects": "#",
-    "Coming Soon": "#"
+    "Paste Bin": "http://kais.local:5050/",
+    "Weather Dashboard": "http://kais.local:89",
+    "Quran 1": "https://soundcloud.com/user-770372009/sets/quran/s-zgvBuYYwQOt",
+    "Quran 2": "https://soundcloud.com/s-a-550587357/ot51jzawhoks"
 };
